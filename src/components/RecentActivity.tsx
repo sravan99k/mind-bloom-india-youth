3a8916f8acc54c682f8e86f563ae7217b3954ae7
vathsal14 <150212169+sravan99k@users.noreply.github.com>
2025-07-17 13:08:12 +0530

    hi

--- src/components/RecentActivity.tsx
+++
@@ -1,4 +0,0 @@
-// Deprecated: This component is no longer used. Dashboard now uses PersonalizedDashboard for real-time data.
-const RecentActivity = () => null;
-
-export default RecentActivity;
--- src/pages/safety/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all safety components
-export { default as AcademicPressureSafety } from './AcademicPressureSafety';
-export { default as CyberbullyingSafety } from './CyberbullyingSafety';
-export { default as SubstanceAbuseViolenceAwareness } from './SubstanceAbuseViolenceAwareness';
-export { default as SexualHarassmentAwareness } from './SexualHarassmentAwareness';
